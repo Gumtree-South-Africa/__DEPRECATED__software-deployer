e4f708ff23cc2049314d3787e30cf3221d3d11eb
Patrick McConnell <pmcconnell@ebay.com>
2014-09-23 17:20:54 +0200

    More detail for CommandLine class

--- deployerlib/commandline.py
+++ deployerlib/commandline.py
@@ -7,20 +7,23 @@ from deployerlib.exceptions import DeployerException
 class CommandLine(object):
     """Handle the command line of front-end scripts"""
 
-    def __init__(self, parents=[]):
+    def __init__(self, parents=[], require_component=True, require_host=False):
         parser = argparse.ArgumentParser(parents=parents)
 
-        parser.add_argument('-d', '--debug', action='store_true')
-        parser.add_argument('-v', '--verbose', action='store_true')
+        output_group = parser.add_mutually_exclusive_group()
+        output_group.add_argument('-v', '--verbose', action='store_true', help='Show more information')
+        output_group.add_argument('-d', '--debug', action='store_true', help='Show a lot more information')
 
-        parser.add_argument('-c', '--config')
+        parser.add_argument('-c', '--config', required=True, help='Specify a platform config file')
 
-        parser.add_argument('--component')
-        parser.add_argument('--directory')
+        component_group = parser.add_mutually_exclusive_group(required=require_component)
+        component_group.add_argument('--component', help='Specify a single component to deploy')
+        component_group.add_argument('--directory', help='Specify a directory of components to deploy')
 
-        parser.add_argument('--cluster')
-        parser.add_argument('--host')
+        host_group = parser.add_mutually_exclusive_group(required=require_host)
+        host_group.add_argument('--cluster', help='Specify a cluster of hosts to deploy to')
+        host_group.add_argument('--host', help='Specify a single host to deploy to')
 
-        parser.add_argument('--redeploy')
+        parser.add_argument('--redeploy', action='store_true', help='Redeploy services even if they exist on remote hosts')
 
         parser.parse_args(namespace=self)
