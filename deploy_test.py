2b69627ce64e7369dd3394f0fad02d7cce63d459
Patrick McConnell <pmcconnell@ebay.com>
2014-10-28 11:46:51 +0100

    Handle DeployerException from Executor.run() in the top-level script

--- deploy_test.py
+++ deploy_test.py
@@ -1,16 +1,22 @@
 #! /usr/bin/python
 
+import os
 import argparse
 
+from deployerlib.log import Log
 from deployerlib.commandline import CommandLine
 from deployerlib.config import Config
 from deployerlib.executor import Executor
+from deployerlib.exceptions import DeployerException
 
-# Add command line option for components to deploy
 parser = argparse.ArgumentParser()
 parser.add_argument('--tasklist', help='A list of pre-generated tasks', required=True)
 
+log = Log(os.path.basename(__file__))
 args = CommandLine(parents=parser, require_config=False)
-
 executor = Executor(args.tasklist)
-executor.run()
+
+try:
+    executor.run()
+except DeployerException as e:
+    log.critical('Execution failed: {0}'.format(e))
