91585129c1eed8c3a692a39bbd83530243fe9aad
Patrick McConnell <pmcconnell@ebay.com>
2014-12-08 17:53:00 +0100

    Add a log message every 30 seconds for services that take a long time to stop or start
    To prevent user confusion when there is no output for a long period of time.

--- deployerlib/commands/checkservice.py
+++ deployerlib/commands/checkservice.py
@@ -5,23 +5,29 @@ from deployerlib.command import Command
 class CheckService(Command):
     """Probe a service to make sure it's in the correct state"""
 
-    def initialize(self, remote_host, check_command, want_state=0, timeout=60):
+    def initialize(self, remote_host, check_command, want_state=0, timeout=60, notify_interval=30):
         self.want_state = want_state
         self.timeout = timeout
+        self.notify_interval = notify_interval
         return True
 
     def execute(self):
         """Probe a service to make sure it's in the correct state"""
 
-        maxtime = time.time() + self.timeout
+        last_notify = time.time()
+        max_time = time.time() + self.timeout
         success = False
 
-        while time.time() < maxtime:
+        while time.time() < max_time:
             res = self.remote_host.execute_remote(self.check_command)
 
             if res.return_code == self.want_state:
                 success = True
                 break
+
+            if self.notify_interval and (time.time() - last_notify) > self.notify_interval:
+                self.log.info('Waiting for service to enter correct state')
+                last_notify = time.time()
 
             time.sleep(1)
 
