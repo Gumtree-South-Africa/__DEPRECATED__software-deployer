1bcd979e2ce7deb4e3a96dd4f21a5fb0ea45e8d9
Yevgen Flerko <yflerko@ebay.com>
2015-04-29 01:03:22 +0200

    some comments and examples

--- deployerweb/views.py
+++ deployerweb/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from loggers import get_logger
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user
 from django.conf import settings
 
 logger = get_logger('view_logger')
@@ -24,8 +24,8 @@ def authorize(request):
     ''' Where real authorization done '''
 
     request_context = RequestContext(request)
-    print request.POST
-    print request.GET
+    # print request.POST
+    # print request.GET
 
     user = authenticate(username=request.POST.get('username', None), password=request.POST.get('password', None))
     if user is not None:
@@ -52,7 +52,19 @@ def list_configs(request):
     ''' Primary page after user successfully authorized '''
 
     request_context = RequestContext(request)
+    # Example to how get some data about user by request dispatch
+    # user.groups return django.db.models.query.QuerySet:
+    # https://docs.djangoproject.com/en/1.8/ref/models/querysets/#values-list
+    user = get_user(request)
+    print user.groups.all().values_list('name', flat=True)
+    print user.groups.all()[0]
+    # print user.get_group_permissions()
+    # print user.user_permissions.all()
+    # if user.groups.filter(name='icas').exists():
+    #    print "in group icas"
+    # user.groups.filter(name__in=['group1', 'group2']).exists()
+
     if request.method == 'GET':
         # configs = [x for x in os.listdir('/etc') if x.endswith('.yaml')]
-        configs = [x for x in os.listdir('etc/') if x.endswith('')]
+        configs = [x for x in os.listdir('mp-conf/') if x.endswith('')]
         return render_to_response('list_configs_new.html', {'configs': configs}, context_instance=request_context)
