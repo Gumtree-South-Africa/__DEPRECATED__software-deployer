7d7a7b4bfee06db14c4895230119de910e7b35f2
Yevgen Flerko <yflerko@ebay.com>
2015-05-13 15:09:31 +0200

    hate pem8 :)

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    # if os.environ.get("DJANGO_SETTINGS_MODULE")
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deployerweb.settings")
 
     from django.core.management import execute_from_command_line
--- setup.py
+++ setup.py
@@ -5,19 +5,21 @@ from setuptools import setup
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
+
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
-    name = "software-deployer",
-    version = "0.9",
-    author = "Patrick McConnell",
-    author_email = "pmcconnell@ebay.com",
-    description = ("A modular, extendable, configuration-driven deployment system."),
-    license = "GPL",
-    keywords = "python software deployment",
-    url = "https://github.scm.corp.ebay.com/ecg-marktplaats/software-deployer",
+    name="software-deployer",
+    version="0.9",
+    author="Patrick McConnell",
+    author_email="pmcconnell@ebay.com",
+    description=("A modular, extendable, configuration-driven deployment system."),
+    license="GPL",
+    keywords="python software deployment",
+    url="https://github.scm.corp.ebay.com/ecg-marktplaats/software-deployer",
     packages=['deployerlib', 'deployerlib.commands', 'deployerlib.generators'],
     long_description=read('README.md'),
     classifiers=[
@@ -36,8 +38,8 @@ setup(
         "yaml"
     ],
     scripts=[
-      'bin/deploy.py',
-      'bin/build_tasklist.py',
-      'bin/servicecontrol.py',
+        'bin/deploy.py',
+        'bin/build_tasklist.py',
+        'bin/servicecontrol.py',
     ],
 )
