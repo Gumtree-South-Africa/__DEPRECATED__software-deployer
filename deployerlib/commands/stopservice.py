Gumtree-South-Africa/__DEPRECATED__software-deployer

commit 0b5e1ef5fd98e073c360e6db97ac841ac7e19aa6

Add ability to force-kill a service
To force kill a service, add a status_command and kill_command to the service config

deployerlib/commands/stopservice.py
@@ -6,11 +6,14 @@ from deployerlib.commands.checkservice import CheckService
 class StopService(Command):
     """Stop a service and check to make sure it has been stopped successfully"""
 
-    def initialize(self, remote_host, stop_command, check_command=None, timeout=60):
+    def initialize(self, remote_host, stop_command, check_command=None, status_command=None, kill_command=None, timeout=60):
 
         self.controlservice = ControlService(
           remote_host=self.remote_host,
           control_command=self.stop_command,
+          status_command=self.status_command,
+          kill_command=self.kill_command,
+          want_state='down',
           tag=self.tag,
         )
 

deployerlib/commands/controlservice.py
@@ -1,10 +1,16 @@
+import time
+
 from deployerlib.command import Command
 
 
 class ControlService(Command):
     """Stop, start and check a remote service"""
 
-    def initialize(self, remote_host, control_command):
+    def initialize(self, remote_host, control_command, status_command=None, kill_command=None, want_state=None, timeout=60):
+        self.status_command = status_command
+        self.kill_command = kill_command
+        self.want_state = want_state
+        self.timeout = timeout
         return True
 
     def execute(self):
@@ -17,4 +23,51 @@ class ControlService(Command):
             self.log.critical('Failed to control service: {0}'.format(res))
             return False
 
+        # Wait for service to be down if status_command is specified
+        if self.status_command and self.want_state and not self.wait(self.want_state, self.timeout):
+
+            if self.want_state == 'down' and self.kill_command:
+                self.log.warning('Service did not respond to TERM, trying KILL')
+                res = self.remote_host.execute_remote(self.kill_command, use_sudo=True)
+
+                if not self.wait('down', self.timeout):
+                    self.log.critical('Failed to KILL service')
+                    return False
+            else:
+                self.log.critical('Failed to stop service')
+                return False
+
         return True
+
+    def check(self):
+        """Check status of a daemontools service"""
+
+        self.log.debug('Checking daemontools service: {0}'.format(self.status_command))
+        res = self.remote_host.execute_remote(self.status_command, use_sudo=True)
+
+        if not res.succeeded:
+            self.log.critical('Failed to check daemontools service: {0}'.format(res))
+            return False
+
+        # return first word of status (normally 'up' or 'down')
+        if ':' in res:
+            return res.split(':')[1].split()[0]
+
+        return False
+
+    def wait(self, want_state, timeout):
+        """Wait for a daemontools service to enter a given state"""
+
+        self.log.debug('Waiting {0} seconds for service to come {1}'.format(timeout, want_state))
+        max_time = time.time() + timeout
+
+        while time.time() < max_time:
+
+            if self.check() == want_state:
+                self.log.debug('Service is "{0}"'.format(want_state))
+                return True
+
+            time.sleep(1)
+
+        self.log.debug('Service did not come {0} within {1} seconds'.format(want_state, timeout))
+        return False

deployerlib/commands/deployandrestart.py
@@ -7,8 +7,9 @@ class DeployAndRestart(Command):
     """Meta-command that includes load balancer control, service control and service activation"""
 
     def initialize(self, remote_host, source, destination=None, stop_command=None, start_command=None,
-      reload_command=None, link_target=None, check_command=None, control_timeout=60, check_daemontools=True,
-      check_registered=False, abort_on_failed_precheck=False,
+      reload_command=None, link_target=None, check_command=None, control_timeout=60,
+      status_command=None, kill_command=None,
+      check_daemontools=True, check_registered=False, abort_on_failed_precheck=False,
       lb_hostname=None, lb_username=None, lb_password=None, lb_service=None, lb_timeout=60):
 
         if not destination:
@@ -50,6 +51,8 @@ class DeployAndRestart(Command):
             self.subcommands.append(stopservice.StopService(
               remote_host=remote_host,
               stop_command=stop_command,
+              status_command=status_command,
+              kill_command=kill_command,
               check_command=check_command,
               timeout=control_timeout,
               tag=self.tag,

deployerlib/generators/icas.py
@@ -248,10 +248,8 @@ class IcasGenerator(Generator):
                     if hasattr(service_config, option):
                         deploy_task[option] = getattr(service_config, option)
 
-                control_commands = ['stop_command', 'start_command', 'check_command']
-
                 # add service control options
-                for cmd in control_commands:
+                for cmd in ['stop_command', 'start_command', 'check_command', 'status_command', 'kill_command']:
 
                     if hasattr(service_config, cmd):
                         deploy_task[cmd] = service_config[cmd].format(

etc/sampleconfig-fakeicas.conf
@@ -54,6 +54,8 @@ ignore_packages:
 daemontools_defaults:
     stop_command: /usr/bin/svc -d /etc/service/{servicename}
     start_command: /usr/bin/svc -u /etc/service/{servicename}
+    status_command: /usr/bin/svstat /etc/service/{servicename}
+    kill_command: /usr/bin/svc -k /etc/service/{servicename}
 
 http_defaults:
     check_command: /usr/lib/nagios/plugins/check_http -I localhost -u http://localhost:{port}/ -p {port} -e "200,301,302,303,307,403,404"

commit d3857c59fcc1e34867878a9994972afe400d237a

Fix bug in StopService
Introduced in 0b5e1ef5

deployerlib/commands/stopservice.py
@@ -11,8 +11,8 @@ class StopService(Command):
         self.controlservice = ControlService(
           remote_host=self.remote_host,
           control_command=self.stop_command,
-          status_command=self.status_command,
-          kill_command=self.kill_command,
+          status_command=status_command,
+          kill_command=kill_command,
           want_state='down',
           tag=self.tag,
         )