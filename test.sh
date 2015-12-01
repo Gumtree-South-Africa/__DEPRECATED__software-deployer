6f714b37985610ec91ff5605962e764a6a0907f1
Ivan Shubin <ishubin@ebay.com>
2015-12-01 12:05:32 +0100

    Added script to run all tests

---
+++ test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set +e
+export PYTHONPATH=`pwd`
+
+tests=(
+    "deployerlib/tests/aurora_generator_test.py"
+    "deployerlib/tests/auroraint_generator_test.py"
+    "deployerlib/tests/createdeploypackagetest.py"
+    "deployerlib/tests/deploymonitor_notify_test.py"
+    "deployerlib/tests/deploymonitor_upload_test.py"
+    "deployerlib/tests/createdeploypackagetest.py"
+    "deployerlib/tests/local_createdirectory_test.py"
+    "deployerlib/tests/pipeline_upload_test.py"
+)
+
+failure=0
+failed_tests=()
+
+for test in ${tests[@]}; do
+    echo Testing $test
+    python $test
+    if [ "$?" -ne "0" ]; then
+        failure=1
+        failed_tests+=($test)
+    fi
+done
+
+
+if [ "$failure" -ne "0" ]; then
+    echo ""
+    echo "==================="
+    echo "-------------------"
+    echo "[TESTS FAILED]"
+    echo "-------------------"
+    echo "There were failures in:"
+    for test in ${failed_tests[@]}; do
+        echo "     $test"
+    done
+fi
