e7ff389f3d826896f832c2e757c22e90cbb94fb1
Yevgen Flerko <yflerko@ebay.com>
2015-12-18 11:34:23 +0100

    remove deploymentmonitor-notify

--- deployerlib/generators/aanbieding.py
+++ deployerlib/generators/aanbieding.py
@@ -34,29 +34,4 @@ class AanbiedingGenerator(Generator):
             if self.config.get('history'):
                 self.archive_stage()
 
-            if self.config.get('deploy_monitor_url'):
-                self.use_deploy_monitor(self.release_version, self.config.environment)
-
         return self.tasklist.generate()
-
-
-    def use_deploy_monitor(self, release_version, environment):
-        self.deploy_monitor_notify('deploying', release_version)
-        self.deploy_monitor_notify('deployed', release_version)
-
-
-    def deploy_monitor_notify(self, status, release_version):
-        self.log.info('Creating task for: notify deployment monitor with status {0} for {1}'.format(status, release_version))
-
-        deploy_monitor_url = self.config.get('deploy_monitor_url')
-
-        stage_name = 'Pipeline notify {0}'.format(status)
-        self.tasklist.create_stage(stage_name)
-        self.tasklist.add(stage_name, {
-            'command': 'deploymonitor_notify',
-            'url': deploy_monitor_url,
-            'release_version': release_version,
-            'environment':self.config.get('environment'),
-            'status': status,
-            'proxy': self.config.get('proxy'),
-        })
